cdf8fbfc10116bb5774e97beea5c2cd8f63028a5
Gustavo Costa <cmtehenz@gmail.com>
2021-06-10 13:27:10 -0300

    :tada: _Componente:header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
+import { stringify } from 'querystring';
 import styles from './styles.module.scss';
 
 export function Header (){
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/imagens/logo.svg" alt="logo" />
+        <img src="/images/logo.svg" alt="logo" />
         <nav>
-          <a>Home</a>
+          <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
       </div>
